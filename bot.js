8c53210bf7a9e1c61cf276fa948a410c1823a44e
umarrg <umarabubakarfta@gmail.com>
2024-10-31 10:42:09 +0100

    added some fixes

--- bot.js
+++ bot.js
@@ -1,38 +1,46 @@
 require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
-const { Client, GatewayIntentBits, Partials } = require('discord.js');
+const { Client, GatewayIntentBits, Partials, Events, PermissionsBitField } = require('discord.js');
 const axios = require('axios');
 const express = require('express');
 
 const app = express();
+const bot = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+
+    ],
+    partials: [
+        Partials.Channel,
+        Partials.Message
+    ]
+
+});
+const chatIds = new Set();
 
 const telegramBot = new TelegramBot(process.env.TOKEN, { polling: true });
+
 const telegramChatId = process.env.CHAT_ID;
 telegramBot.setMyCommands([
     { command: '/start', description: 'Start the bot and get welcome message' },
     { command: '/help', description: 'Get instructions on how to use the bot' },
 
 ]);
-const discordClient = new Client({
-    allowedMentions: { parse: [] },
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildPresences,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.GuildMessageReactions,
-        GatewayIntentBits.DirectMessages,
-        GatewayIntentBits.DirectMessageReactions,
-        GatewayIntentBits.MessageContent,
-    ],
-    partials: [
-        Partials.Channel,
-        Partials.Message
-    ],
-});
-// const discordClient = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent, GatewayIntentBits.DirectMessages, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions], partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
+
+
 const discordWebhookUrl = process.env.DISCORD_WEBHOOK_URL;
 const discordChannelId = process.env.DISCORD_CHANNEL_ID;
 telegramBot.onText(/\/start/, (msg) => {
+    const chatId = msg.chat.id;
+
+    if (!chatIds.has(chatId)) {
+        chatIds.add(chatId);
+        console.log(`Added chatId ${chatId} to notification list.`);
+    } else {
+    }
     const welcomeMessage = `
     🤖 *Welcome to the Telegram-Discord Forwarder Bot* 🤖
     
@@ -44,7 +52,9 @@ telegramBot.onText(/\/start/, (msg) => {
    
     `;
     telegramBot.sendMessage(msg.chat.id, welcomeMessage, { parse_mode: 'Markdown' });
+
 });
+
 
 telegramBot.onText(/\/help/, (msg) => {
     const helpMessage = `
@@ -71,7 +81,14 @@ const forwardToDiscord = async (message) => {
 
 const forwardToTelegram = async (message) => {
     try {
-        await telegramBot.sendMessage(telegramChatId, message);
+        for (const chatId of chatIds) {
+            try {
+                await telegramBot.sendMessage(chatId, message);
+
+            } catch (error) {
+                console.error(`Error sending message to chatId ${chatId}:`, error);
+            }
+        }
     } catch (error) {
         console.error('Error forwarding to Telegram:', error.message);
     }
@@ -87,24 +104,24 @@ telegramBot.on('message', (msg) => {
         .then(() => console.log(`Message from Telegram forwarded to Discord: ${message}`))
         .catch(console.error);
 });
-discordClient.on('ready', () => {
-    console.log('FINALLYYYYYY')
-})
-discordClient.on('messageCreate', (message) => {
-    console.log('Message event triggered');
-    if (message.author.bot || !message.content) return;
+bot.on('messageCreate', (message) => {
 
-    const username = message.author.username;
-    const content = message.content;
-    const formattedMessage = `💬 *${username}*: ${content}`;
-
-    forwardToTelegram(formattedMessage)
-        .then(() => console.log(`Message from Discord forwarded to Telegram: ${formattedMessage}`))
-        .catch(console.error);
+    if (message.author.bot) return;
+    forwardToTelegram(message.content)
 });
 
 
-discordClient.login(process.env.DISCORD_KEY);
+bot.on('error', console.log);
+
+bot.on('ready', () => {
+    console.log('FINALLYYYYYY')
+})
+
+bot.once(Events.ClientReady, readyClient => {
+    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+bot.login(process.env.DISCORD_KEY);
+
 
 telegramBot.on('polling_error', (error) => console.error('Telegram Polling Error:', error));
 
